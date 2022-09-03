758a120e128d708276a66f333ed4c5cbdead780b
alijm0806 <alijm1104@gmail.com>
2022-09-03 08:36:50 -0500

    Add weather-wrap and weather-box hardcoded + styling

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -26,6 +26,22 @@ export default {
           <div class="search-box">
             <input type="text" class="search-bar" placeholder="Search...">
           </div>
+          <div class="weather-wrap">
+            <div class="location-box">
+              <div class="location">Austin, TX</div>
+              <div class="date">saturday 3 september 2022</div>
+            </div>
+
+            <div class="weather-box">
+              <div class="temp">9°c</div>
+              <div class="weather">Rain</div>
+            </div>
+          </div>
+
+
+
+
+
         </main>
 
 
@@ -87,5 +103,52 @@ main {
   background-color: rgba(255, 255, 255, 0.5);
   border-radius: 0px 16px 0px 16px;
   transition: 0.4s;
+}
+
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+
+.location-box .location {
+  color: #FFF;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+
+.location-box .date {
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #FFF;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+
+.weather-box .weather {
+  color: #FFF;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
 }
 </style>
